209fb9f32b6d621eb5c17d860096ca68da41a766
RiccardoMiolato <riccardomiolato@gmail.com>
2026-04-20 18:15:17 +0200

    Change: How the agent move around and more reactive behaviour

--- agent/agent.ts
+++ agent/agent.ts
@@ -1,6 +1,6 @@
 import { Astar, Position } from "./astar.js";
 import beliefs from "./beliefs.js";
-import { getDirection, getNextParcel } from "./utils.js";
+import { getDirection, getNextParcel, getClosestDeliveringCell } from "./utils.js";
 import socket from "../index.js";
 
 /**
@@ -31,7 +31,16 @@ class Agent {
     async makeMove() {
         for (const parcel of beliefs.parcels.values()) {
             const parcel_pos = new Position(parcel.x, parcel.y);
-            if(this.position.isEqual(parcel_pos)) {
+
+            if(beliefs.state == 0 && beliefs.target_parcel == undefined && parcel.carriedBy == null){
+                beliefs.target_pos = getNextParcel();
+
+                if (beliefs.target_pos != undefined) {
+                    beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
+                }
+            }
+
+            if(this.position.isEqual(parcel_pos) ) {
                 await socket.emitPickup();
             }
         }
@@ -59,17 +68,28 @@ class Agent {
                             // If after few tries I can't reach my goal, I have to recalculate the
                             // path considering the position blocked by an agent or whatever
                             if(!result) {
-                                beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
+                                beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates, next_position);
                             }
                         }
                     }
                 }
             } else if (beliefs.state == 0){
-                await socket.emitPickup();
+                if(beliefs.target_parcel != undefined){
+                    await socket.emitPickup();
 
-                beliefs.target_parcel = undefined;
-                beliefs.state = 1;
-                beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.delivering_cells[0], beliefs.crates);
+                    beliefs.target_parcel = undefined;
+                    beliefs.state = 1;
+                    beliefs.target_pos = getClosestDeliveringCell(this.position, beliefs.delivering_cells);
+
+                    if (beliefs.target_pos){
+                        beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
+                    }
+                } else {
+                    beliefs.target_pos = getNextParcel();
+
+                    if(beliefs.target_pos)
+                        beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
+                }
             } else if (beliefs.state == 1){
                 await socket.emitPutdown();
                 beliefs.clearDeliveredParcels();
@@ -83,6 +103,7 @@ class Agent {
             }
         } else {
             beliefs.target_pos = getNextParcel();
+
             if (beliefs.target_pos) {
                 beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
             }
