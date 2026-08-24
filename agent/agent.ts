457f4136607246babea67e4daf187a88abc6cbbb
RiccardoMiolato <riccardomiolato@gmail.com>
2026-08-24 17:53:30 +0200

    Agent: clear agent structure from state machine

--- agent/agent.ts
+++ agent/agent.ts
@@ -1,7 +1,6 @@
-import { Astar, Position } from "./astar.js";
+import { Position } from "./astar.js";
 import beliefs from "./beliefs.js";
-import { getDirection, getNextParcel, getClosestDeliveringCell } from "./utils.js";
-import socket from "../index.js";
+import { Intention } from "./intentions.js";
 
 /**
  * Class that manages the main agent logic
@@ -12,9 +11,13 @@ class Agent {
     id: string;
     position: Position;
 
+    intentions: Intention[];
+
     constructor() {
         this.id = "";
         this.position = new Position(0,0); // Initialize beliefs with default values
+
+        this.intentions = [];
     }
 
     updatePosition(x: number, y: number): void{
@@ -23,112 +26,11 @@ class Agent {
     }
 
     /**
-     * Responsible for the decision of the next move:
-     *     - Where to move
-     *     - If to pick a parcel
-     *     - If to drop a parcel
-     */
-    async makeMove() {
-        // Check if the current position contains a parcel
-        for (const parcel of beliefs.parcels.values()) {
-            const parcel_pos = new Position(parcel.x, parcel.y);
-
-            if(beliefs.state == 0 && beliefs.target_parcel == undefined && parcel.carriedBy == null){
-                beliefs.target_pos = getNextParcel();
-
-                if (beliefs.target_pos != undefined) {
-                    beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
-                }
-            }
-
-            if(this.position.isEqual(parcel_pos) ) {
-                await socket.emitPickup();
-            }
-        }
-
-        if(this.position.x % 1 === 0 && this.position.y % 1 === 0 && beliefs.target_pos != undefined && beliefs.followed_path != null) {
-            if (beliefs.followed_path.length > 0) {
-                const next_position = beliefs.followed_path.shift();
-
-                if (next_position != undefined) {
-                    const direction = getDirection(this.position, next_position);
-
-                    if (direction != ''){
-                        let result = await socket.emitMove(direction);
-
-                        if(!result) {
-                            for (let i = 0; i < 3; i++) {
-                                const retry_res = await socket.emitMove(direction);
-
-                                if(retry_res) {
-                                    result = true;
-                                    break;
-                                }
-                            }
-
-                            // If after few tries I can't reach my goal, I have to recalculate the
-                            // path considering the position blocked by an agent or whatever
-                            if(!result) {
-                                beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates, next_position);
-
-                                if (beliefs.followed_path.length == 0 && beliefs.state == 1) {
-                                    beliefs.target_pos = getClosestDeliveringCell(this.position, beliefs.delivering_cells, next_position);
-                                    beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos!, beliefs.crates, next_position);
-                                }
-                            }
-                        }
-                    }
-                }
-            } else if (beliefs.state == 0){
-                if(beliefs.target_parcel != undefined){
-                    await socket.emitPickup();
-
-                    beliefs.target_parcel = undefined;
-                    beliefs.state = 1;
-                    beliefs.target_pos = getClosestDeliveringCell(this.position, beliefs.delivering_cells);
-
-                    if (beliefs.target_pos){
-                        beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
-                    }
-                } else {
-                    beliefs.target_pos = getNextParcel();
-
-                    if(beliefs.target_pos)
-                        beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
-                }
-            } else if (beliefs.state == 1){
-                if(beliefs.map[this.position.x][this.position.y] == '2'){
-                    await socket.emitPutdown();
-                    beliefs.clearDeliveredParcels();
-
-                    beliefs.state = 0;
-                    beliefs.target_pos = getNextParcel();
-
-                    if (beliefs.target_pos != undefined) {
-                        beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
-                    }
-                } else {
-                    beliefs.target_pos = getClosestDeliveringCell(this.position, beliefs.delivering_cells);
-                    beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos!, beliefs.crates);
-                }
-            }
-        } else {
-            beliefs.target_pos = getNextParcel();
-
-            if (beliefs.target_pos) {
-                beliefs.followed_path = Astar(beliefs.map, new Position(this.position.x, this.position.y), beliefs.target_pos, beliefs.crates);
-            }
-        }
-    }
-
-    /**
-     * Agent loop
+     * Main agent's logic loop
      */
     async agent_loop() {
         while(true) {
             await new Promise(r => setTimeout(r, beliefs.movement_duration));
-
-            await this.makeMove();
         }
     }
 }
