72e92d72428b6ba09704eb75cea5bb5d0814934c
RiccardoMiolato <riccardomiolato@gmail.com>
2026-08-24 23:04:57 +0200

    Intentions: Add score concept and log

--- agent/intentions.ts
+++ agent/intentions.ts
@@ -1,5 +1,7 @@
+import agent from "./agent.js";
 import { Position } from "./astar.js";
-import { Parcel } from "./beliefs.js";
+import beliefs, { Parcel } from "./beliefs.js";
+import { getClosestDeliveringCell } from "./utils.js";
 
 export enum IntentionType {
     Null,
@@ -17,6 +19,18 @@ export class Intention {
     constructor(intentionType: IntentionType = IntentionType.Null) {
         this.intentionType = intentionType;
     }
+
+    getType(): IntentionType {
+        return this.intentionType;
+    }
+
+    score(): number {
+        throw new Error("Method not implemented.");
+    }
+
+    log() {
+        console.log("Null intention");
+    }
 }
 
 /**
@@ -30,6 +44,12 @@ export class SearchIntention extends Intention {
     constructor() {
         super(IntentionType.SearchPacket);
     }
+
+    score(): number { return 0 };
+
+    log() {
+        console.log("SearchPacket")
+    }
 }
 
 /**
@@ -39,13 +59,35 @@ export class SearchIntention extends Intention {
  * and pick it up
  */
 export class PickUpParcelIntention extends Intention {
-    parcel: Parcel; // TODO: check if both are usefull or only one can work
+    parcel: Parcel;
     parcelPosition: Position;
 
     constructor(parcel: Parcel, parcelPosition: Position) {
         super(IntentionType.PickUpPacket);
         this.parcelPosition = parcelPosition;
         this.parcel = parcel;
+    }
+
+    /**
+     * Idea: I compute how much I gain by picking up and delivering a certain parcel
+     */
+    score(): number {
+        const parcelDistance = this.parcelPosition.distanceTo(agent.position);
+
+        const closestDeliveryFromParcel = getClosestDeliveringCell(this.parcelPosition, beliefs.delivering_cells, beliefs.crates.values().next().value);
+        if (closestDeliveryFromParcel !== undefined){
+            const deliveryDistance = this.parcelPosition.distanceTo(closestDeliveryFromParcel);
+
+            const timeToDeliver = ((parcelDistance + deliveryDistance) * beliefs.movement_duration) / 1000.0;
+            if (this.parcel.reward > timeToDeliver)
+                return this.parcel.reward - timeToDeliver;
+        }
+
+        return -1;
+    }
+
+    log() {
+        console.log(`PickUp packet from (${this.parcelPosition.x};${this.parcelPosition.y})`);
     }
 }
 
@@ -61,5 +103,28 @@ export class DeliverParcelIntention extends Intention {
     constructor(deliveryCell: Position) {
         super(IntentionType.DeliverPacket);
         this.deliveryCell = deliveryCell;
+    }
+
+    /**
+     * Idea: calculate how much points I get by delivering current parcels
+     */
+    score(): number {
+        const closestDeliveryFromParcel = getClosestDeliveringCell(agent.position, beliefs.delivering_cells, beliefs.crates.values().next().value);
+
+        if (closestDeliveryFromParcel !== undefined){
+            const timeToDeliver = agent.position.distanceTo(closestDeliveryFromParcel) * beliefs.movement_duration;
+
+            let reward = 0;
+
+            agent.getCarryingParcels().forEach(parcel => reward += Math.max(0, parcel.reward - timeToDeliver))
+
+            return reward;
+        }
+
+        return -1;
+    }
+
+    log() {
+        console.log(`Delivering packet at (${this.deliveryCell.x};${this.deliveryCell.y})`);
     }
 }
