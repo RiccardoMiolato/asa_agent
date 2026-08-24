6b7f266549a519313e6f34589b1dcdca0640de95
RiccardoMiolato <riccardomiolato@gmail.com>
2026-08-24 23:06:55 +0200

    Removed unused function

--- agent/utils.ts
+++ agent/utils.ts
@@ -1,5 +1,4 @@
 import { Position } from "./astar.js";
-import beliefs from "./beliefs.js";
 
 /**
  * Returns the string of the direction the agent is moving to,
@@ -18,38 +17,6 @@ export function getDirection(actual_pos: Position, next_pos: Position): String {
     }
 
     return '';
-}
-
-/**
- * Decides which is the best parcel to pick next
- * If i don't know valid parcel positions around the map, I will move
- * to a random parcel spawn point, hoping to find a parcel
- */
-export function getNextParcel(): Position | undefined {
-    if (beliefs.parcels.size == 0) {
-        beliefs.target_parcel = undefined;
-
-        if (beliefs.pickup_cells.length > 0) {
-            const index = Math.floor(Math.random() * beliefs.pickup_cells.length);
-            return beliefs.pickup_cells[index];
-        }
-
-        return undefined;
-    } else if (beliefs.target_pos == undefined && beliefs.state == 0) {
-        for (const parcel of beliefs.parcels.values()) {
-            if(parcel.carriedBy == null) {
-                beliefs.target_parcel = parcel.id;
-                return new Position(parcel.x, parcel.y);
-            }
-        }
-
-        if (beliefs.pickup_cells.length > 0) {
-            const index = Math.floor(Math.random() * beliefs.pickup_cells.length);
-            return beliefs.pickup_cells[index];
-        }
-    }
-
-    return undefined;
 }
 
 
